68c585902f69a9cf8a696992431a7a22bf08f733
Dmitry913HUB <58403614+Dmitry913HUB@users.noreply.github.com>
2021-12-08 05:40:57 +0300

    -fix and deploy rarity

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 500,
+        responsive: [
+            {
+                breakpoint: 0,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 10,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+const element = $('.js-tilt').tilt();
+
