9c802b577b9b1e7c95a0595bc4fe037b03bdb05b
Dmitry913HUB <58403614+Dmitry913HUB@users.noreply.github.com>
2022-01-06 19:33:51 +0300

    -complite

--- js/script.js
+++ js/script.js
@@ -1,8 +1,56 @@
 $(document).ready(function () {
-    $('.slider').slick({
+    $('.slider5').slick({
         arrows: true,
         dots: true,
         slidesToShow: 5,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 500,
+        responsive: [
+            {
+                breakpoint: 0,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 10,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+$(document).ready(function () {
+    $('.slider4').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 4,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 500,
+        responsive: [
+            {
+                breakpoint: 0,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 10,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+$(document).ready(function () {
+    $('.slider3').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
         autoplay: true,
         speed: 2000,
         autoplaySpeed: 500,
